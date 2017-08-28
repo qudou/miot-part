303be07169434cd594bd76b6496953c01ff5066a
jianfei <326214104@qq.com>
2017-08-28 12:03:05 +0800

    update

--- musicbox.js
+++ musicbox.js
@@ -205,7 +205,7 @@ $_().imports({
             });
             this.watch("100+", (e, d) => {
                 let now = new Date, year = now.getFullYear(), month = now.getMonth(), day = now.getDate(), week = now.getDay();
-                d.speek = `${year}年${month+1}月${day}日，星期${week}`;
+                d.speek = `${year}年${month+1}月${date}日，星期${week == 0 ? '日' : week}`;
                 this.notify("exec", ["pause speek resume", d]);
             });
             this.watch("200+", (e, d) => {
