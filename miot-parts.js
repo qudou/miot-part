83900f07461d661e8fdbb7f1ca6af1e551c3ee9e
qudou <qudou@xmlplus.cn>
2019-10-27 23:45:28 +0800

    update

--- miot-parts.js
+++ miot-parts.js
@@ -7,6 +7,7 @@
 
 const xmlplus = require("xmlplus");
 const MiotRoot = `${__dirname}/../miot-local/`;
+const Server = "mqtt://raspberrypi:1883";
 const Link = "5971b164-779f-4bfa-a676-16582a77d7e9";
 
 const log4js = require("log4js");
@@ -20,71 +21,63 @@ xmlplus("miot-parts", (xp, $_) => {
 
 $_().imports({
     Index: {
-        xml: "<MQTT id='mqtt'>\
+        xml: "<main id='index'>\
                 <Sqlite id='sqlite'/>\
-              </MQTT>",
-        map: { share: "Sqlite" },
-        fun: async function (sys, items, opts) {
+                <MQTT id='mqtt'/>\
+                <Client id='client'/>\
+              </main>",
+        map: { share: "Sqlite", defer: "client" },
+        fun: function (sys, items, opts) {
             items.sqlite.all(`SELECT * FROM parts`, (err, rows) => {
                 if (err) throw err;
-                rows.forEach(loadPart);
-                items.mqtt.init();
+                rows.forEach(p => p.link == Link && load(p));
+                sys.client.show();
             });
-            function loadPart(part) {
-                if (part.link != Link) return;
-                try {
-                    load(part);
-                    sys.mqtt.append(`<Client id='${part.id}'/>`);
-                } catch(e) {
-                    logger.error(e), console.log(e);
-                }
-            }
             function load(part) {
                 require(`./parts/${part.id}/index.js`);
-                let c = {map: {}, fun: fun};
-                c.xml = `<Index xmlns='//${part.id}'/>`;
+                let c = xp.hasComponent(`//${part.id}/Index`);
                 c.map.msgscope = true;
-                $_().imports({Client: c});
-            }
-            function fun(sys, items, opts) {
-                this.on("/SYS", (e, p) => {
-                    p = JSON.parse(p);
-                    this.notify(p.topic, p.body);
-                });
+                sys.mqtt.append(`<Index id='${part.id}' xmlns='//${part.id}'/>`);
             }
         }
     },
     MQTT: {
-        opt: { server: "mqtt://raspberrypi:1883", clientId: "5971b164-779f-4bfa-a676-16582a77d7e9" },
+        xml: "<main id='mqtt'/>",
         fun: function (sys, items, opts) {
-            let client;
-            const that = this;
+            let table;
             // 此 to-user 用于局域网内配件与视图端之间的通信
             this.on("to-user", "./*[@id]", function (e, topic, data) {
                 e.stopPropagation();
-                let payload = {topic: topic, pid: this.toString(), data: data };
-                client.publish("to-gateway", JSON.stringify(payload), {qos:1,retain: true});
+                let p = {topic: topic, pid: this.toString(), data: data };
+                sys.mqtt.notify("publish", ["to-gateway", p]);
             });
             // 此 to-part 用于局域网内配件之间的通信
-            this.on("to-part", "./*[@id]", function (e, target, payload) {
+            this.on("to-part", "./*[@id]", function (e, target, p) {
                 e.stopPropagation();
-                payload.pid = this.toString();
-                client.publish(target, JSON.stringify(payload), {qos:1,retain: true});
+                p.pid = this.toString();
+                sys.mqtt.notify("publish", [target, p]);
             });
-            function init() {
-                let table = that.children().hash();
-                delete table.sqlite;
-                client  = require("mqtt").connect(opts.server, opts);
-                client.on("connect", e => {
-                    Object.keys(table).forEach(partId => client.subscribe(partId));
-                    console.log("connected to " + opts.server);
-                    logger.info("connected to " + opts.server);
-                });
-                client.on("message", (topic, msg) => {
-                    table[topic] && table[topic].trigger("/SYS", msg, false);
-                });
-            }
-            return { init: init };
+            this.watch("connect", () => {
+                table = table || this.children().hash();
+                Object.keys(table).forEach(partId => this.notify("subscribe", partId));
+                console.log("connected to " + Server);
+            });
+            this.watch("message", (e, topic, p) => {
+                table[topic] && table[topic].notify(p.topic, p.body);
+            });
+        }
+    },
+    Client: {
+        fun: function (sys, items, opts) {
+            let client  = require("mqtt").connect(Server, {clientId: Link});
+            client.on("connect", e => this.notify("connect"));
+            client.on("message", (topic, p) => {
+                this.notify("message", [topic, JSON.parse(p)])
+            });
+            this.watch("publish", (e, topic, p) => {
+                client.publish(topic, JSON.stringify(p), {qos:1,retain: true});
+            });
+            this.watch("subscribe", (e, topic) => client.subscribe(topic));
         }
     },
     Sqlite: {
