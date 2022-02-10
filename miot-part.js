ea67a74c4336cf9243ce7affda461940cf591888
qudou <qudou@xmlplus.cn>
2022-02-10 13:26:56 +0800

    update

--- miot-part.js
+++ miot-part.js
@@ -1,7 +1,7 @@
 /*!
  * miot-part.js v1.1.1
  * https://github.com/qudou/miot-part
- * (c) 2009-2017 qudou
+ * (c) 2017-2022 qudou
  * Released under the MIT license
  */
 
@@ -11,11 +11,11 @@ xmlplus("miot-part", (xp, $_) => {
 
 $_().imports({
     Client: {
-        opt: { server: "mqtt://localhost:1883", pid: "pid" },
+        opt: { port: 1883, partId: "pid", protocol: "mqtt" },
         fun: function (sys, items, opts) {
-            let client  = require("mqtt").connect(opts.server);
+            let client  = require("mqtt").connect(opts);
             client.on("connect", e => {
-                client.subscribe(opts.pid);
+                client.subscribe(opts.partId);
             });
             client.on("message", (topic, p) => {
                 p = JSON.parse(p);
@@ -27,12 +27,12 @@ $_().imports({
             // 将消息发往用户端
             this.on("to-users", (e, topic, data) => {
                 e.stopPropagation();
-                publish("to-gateway", {pid: opts.pid, topic: topic, data: data});
+                publish("to-gateway", {pid: opts.partId, topic: topic, data: data});
             });
             // 将消息发往局域网内配件
             this.on("to-parts", (e, targets, topic, body) => {
                 e.stopPropagation();
-                let body_ = {targets: targets, pid: opts.pid, topic: topic, body: body}
+                let body_ = {targets: targets, pid: opts.partId, topic: topic, body: body}
                 publish("to-parts", body_);
             });
         }
